cdfc50eeda9f80df56d6cb55d00416769fd1273f
Cpt-Umi <umairkhan.2102@gmail.com>
2023-06-02 17:23:05 +0500

    Register Activity Created

--- app/src/main/java/com/example/dedsec/activities/SplashActivity.java
+++ app/src/main/java/com/example/dedsec/activities/SplashActivity.java
@@ -2,7 +2,12 @@ package com.example.dedsec.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.dedsec.RegisterActivity;
+import com.example.dedsec.R;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
